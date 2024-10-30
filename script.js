300ea786aab34ac3957fdfa9d2e73e964852366a
niina112 <91963048+niina112@users.noreply.github.com>
2024-10-30 22:15:18 +0400

    changed colors in js folder

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
- let name = "Nino";
+let name = "Nino";
 let lastName = "Kochkiani";
 let userAge = 21;
 let email = "kochkianinino6@gmail.com";
 let phoneNumber = "597733513";
 let isAdult = true;
-console.log("I am", name, lastName, "My age is", userAge, "This is my Email-", email, "and also my phone number is",phoneNumber,);
 
-console.log(typeof isAdult);
-
-
+console.log(`I am ${name} ${lastName}, My age is ${userAge}, This is my Email: ${email} and also my phone number is %c${phoneNumber}`, "color: red;");
+console.log(`%c${isAdult}`, "color: yellow;");
