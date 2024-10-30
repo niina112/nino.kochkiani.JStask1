1c4a78079e1773e86d81c9dcfb0be601652a791e
niina112 <91963048+niina112@users.noreply.github.com>
2024-10-30 21:25:32 +0400

    created js folder

---
+++ script.js
@@ -0,0 +1,11 @@
+ let name = "Nino";
+let lastName = "Kochkiani";
+let userAge = 21;
+let email = "kochkianinino6@gmail.com";
+let phoneNumber = "597733513";
+let isAdult = true;
+console.log("I am", name, lastName, "My age is", userAge, "This is my Email-", email, "and also my phone number is",phoneNumber,);
+
+console.log(typeof isAdult);
+
+
